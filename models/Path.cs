1665467c52c8eb9823008d8a0c226233f9c601ad
mayapony <mayapony@foxmail.com>
2022-05-03 19:16:23 +0800

    fix🐛:  path struct to class

---
+++ models/Path.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompilerApp.models
+{
+    class Path
+    {
+        public int start { get; set; }
+        public int end { get; set; }
+        public char val { get; set; }
+        /// <summary>
+        /// 构造一条路径
+        /// </summary>
+        /// <param name="start">开始点</param>
+        /// <param name="end">结束点</param>
+        /// <param name="val">路径上的值</param>
+        public Path(int start, int end, char val)
+        {
+            this.start = start;
+            this.end = end;
+            this.val = val;
+        }
+    }
+}
--- models/PathList.cs
+++ models/PathList.cs
@@ -6,26 +6,6 @@ using System.Threading.Tasks;
 
 namespace CompilerApp.models
 {
-    public struct Path
-    {
-        public int start { get; set; }
-        public int end { get; set; }
-        public char val { get; set; }
-        /// <summary>
-        /// 构造一条路径
-        /// </summary>
-        /// <param name="start">开始点</param>
-        /// <param name="end">结束点</param>
-        /// <param name="val">路径上的值</param>
-        public Path(int start, int end, char val)
-        {
-            this.start = start;
-            this.end = end;
-            this.val = val;
-        }
-    }
-
-
     class PathList
     {
         public SortedSet<int> head { get; set; }
